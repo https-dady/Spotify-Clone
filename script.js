97edfad9893e2300e3553ff7809cbaf40808960f
Tarun Saratkar <tarunsaratkar@gmail.com>
2026-01-16 19:52:57 +0530

    Onclick Songs in now playable

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("The Spotify")
+const player = document.getElementById("play");
+const songs = document.querySelectorAll(".img-songs");
+let currentSong = '';
+
+songs.forEach(img =>{
+    img.addEventListener("click",()=>{
+        console.log("The Spotify")
+        let songPath = img.dataset.audio;
+        if(currentSong === songPath){
+            if(player.paused){
+                player.play();
+            }else{
+                player.pause();
+            }
+            return;
+        }
+        player.src = songPath;
+        player.play();
+        currentSong = songPath;
+    })
+} );
